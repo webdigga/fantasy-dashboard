a5abccbfd910debae568bb2063b7e4a073452844
David White <webdigga42@gmail.com>
2019-09-23 18:33:58 +0100

    Adding basic express route and updating reference to main js entry point

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require( 'express');
+const app = express();
+const port = 3000;
+
+app.get( '/', ( req, res ) => {
+    res.send( 'Hello fantasy dashboard!' );
+});
+
+app.listen( port, () => {
+    console.log( `This application is running on port ${ port }` );
+});
