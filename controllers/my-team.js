822184241c678c6ca7ab66a7e06643c3e155645f
David White <webdigga42@gmail.com>
2019-09-29 19:56:11 +0100

    Making simple API request

--- controllers/my-team.js
+++ controllers/my-team.js
@@ -1,3 +1,8 @@
-const myTeam = 'This will need to be the data from the API response';
+const request = require( 'request' );
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+const myTeam = request(url, { json: true }, ( error, res ) => {
+    return error ? error : res;
+});
 
 module.exports = myTeam;
--- routes/my-team.js
+++ routes/my-team.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const myTeam = require( '../controllers/my-team' );
+const asyncMiddleware = require( '../utils/asyncMiddleware' );
 
-router.get('/my-team', ( req, res ) => {
-    res.send( myTeam );
-});
+router.get('/my-team', asyncMiddleware(async ( req, res, next ) => {
+    try {
+        const myTeamResponse = await myTeam;
+        res.json( myTeamResponse );
+    } catch( error ) {
+        res.json( error );
+    }
+}));
 
 module.exports = router;
